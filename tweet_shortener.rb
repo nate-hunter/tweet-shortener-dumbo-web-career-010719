c9c9ab979c89850a483e948ad0cc31169a0625ec
Nate Hunter <hunter.j.nate@gmail.com>
2018-12-28 22:00:25 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -20,7 +20,7 @@ def word_substituter (tweet_string)
   tweet_string.split.map do | each_word |
     # for each word in tweet string, compare it to the keys in the dictionary. If a tweet word is equal to a word in the dictionary,  replace it with the corresponding dictionary value.  
     
-    if dictionary.keys.include?(tweet_string)
+    if dictionary.keys.include?(each_word.downcase) #why the .downcase needed?
     
   end 
   
