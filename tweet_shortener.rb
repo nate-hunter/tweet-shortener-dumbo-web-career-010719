9c66103920cb5964017fb5f55168e5f53a917421
Nate Hunter <hunter.j.nate@gmail.com>
2018-12-28 22:03:54 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -20,7 +20,10 @@ def word_substituter (tweet_string)
   tweet_string.split.map do | each_word |
     # for each word in tweet string, compare it to the keys in the dictionary. If a tweet word is equal to a word in the dictionary,  replace it with the corresponding dictionary value.  
     
-    if dictionary.keys.include?(each_word.downcase) #why the .downcase needed? Cause if 'For' and 'for' compared it would be false?
+    if dictionary.keys.include?(each_word.downcase) 
+      # why .downcase needed? Cause if 'For' and 'for' compared it would be false?
+      # I want to substitute dictionary values for words in the tweet. How?
+      
     
   end 
   
