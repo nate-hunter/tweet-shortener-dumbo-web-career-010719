f0d3ff229cc4ead7bd3115dc8ad55d04d421a952
Nate Hunter <hunter.j.nate@gmail.com>
2018-12-28 22:25:10 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -18,13 +18,16 @@ dictionary = {
 def word_substituter (tweet_string)
   
   tweet_string.split.map do | a_word |
-          # for each word in the tweet string, compare it to the keys in the dictionary. If a tweet word is equal to a word in the dictionary,  replace it with the corresponding dictionary value.  
+          # for a word in the tweet string, compare it to the keys in the dictionary. If a tweet word is equal to a word in the dictionary,  replace it with the corresponding dictionary value.  
     
     if dictionary.keys.include?(a_word.downcase) 
           # why .downcase needed? Cause if 'For' and 'for' compared it would be false?
           # Next I want to substitute the dictionary values for words in the tweet. How?
       a_word = dictionary[a_word.downcase] 
-          # if "included" in the dictionary, the word, as a variable, is set to the dictionary value that corresponds to dictionary's key. what if the word isn't in the dictionary, what happens next? elsif? else?
+          # if "included" in the dictionary, a word, as a variable, is set to the dictionary value that corresponds to dictionary's key. what if the word isn't in the dictionary, what happens next? elsif? else?
+      else 
+        a_word 
+          # so using else - we just return a word. 
           
           
       
